14f4835c989265d8da4512d886d7d313b3ad0a09
mluto <m@luto.at>
2013-01-11 21:41:27 +0100

    Initial code-commit, added readme, chat-dimensions changeable via settings.json

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-ep_resizeChat
-=============
+# ep_resizeChat
+## Function
+Allows you to set the dimensions of the chat using your `settings.json`.
+## Setting
+```JSON
+"ep_resizeChat" : {
+                    "stickyWidth": "185px",
+                    "normalWidth": "180px",
+                    "normalHeight": "200px"
+                  }
+```
+Sub-Settings:
+
+* `stickWidth`, defaults to `185px`, width of the sticky-chat
+* `normalWidth`, defaults to `180px`, width of the normal chat-window
+* `normalHeight`, defaults to `200px`, height of the normal chat-window
---
+++ index.js
@@ -0,0 +1,31 @@
+var settings;
+
+exports.loadSettings = function (hook, context)
+{
+  settings = {};
+  settings.stickyWidth = '185px';
+  settings.normalWidth = '180px';
+  settings.normalHeight = '180px';
+  
+  if(context.settings.ep_resizeChat)
+  {
+    if(context.settings.ep_resizeChat.stickyWidth)
+      settings.stickyWidth = context.settings.ep_resizeChat.stickyWidth;
+    if(context.settings.ep_resizeChat.normalWidth)
+      settings.normalWidth = context.settings.ep_resizeChat.normalWidth;
+    if(context.settings.ep_resizeChat.normalHeight)
+      settings.normalHeight = context.settings.ep_resizeChat.normalHeight;
+  }
+}
+
+exports.eejsBlock_styles = function (hook, context)
+{
+  var resizeChat = '<style type="text/css">\n';
+  resizeChat += '.stickyChat { width: ' + settings.stickyWidth + ' !important; }\n';
+  resizeChat += '#chatbox\n{\n';
+  resizeChat += 'width: ' + settings.normalWidth + ';\n';
+  resizeChat += 'Height: ' + settings.normalHeight + ';\n}\n';
+  resizeChat += '</style>\n';
+  
+  context.content = context.content + resizeChat;
+}
